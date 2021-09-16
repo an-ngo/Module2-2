7c4b049da3801664ae9a8742f89e57c9e0dc9eb1
an.ngo <phowflowf135@gmail.com>
2021-09-16 17:16:21 +0700

    m

--- src/primeNumber.java
+++ src/primeNumber.java
@@ -1,15 +1,10 @@
-
-
-public class twentyPrimeNumber {
+public class primeNumber {
     public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-        int number = 20;
-        //int count =0;
         System.out.println("2");
         System.out.println("3");
         int n=5;
         boolean check=true;
-        while(number>0){
+        while(n<100){
             for(int i=2;i<n/2;i++){
                 if(n%i==0){
                     check = false;
@@ -17,7 +12,7 @@ public class twentyPrimeNumber {
             }
             if(check){
                 System.out.println(n);
-                number-=1;
+
             }
             n++;
             check = true;
