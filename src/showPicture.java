3617184c3ca8e988687ef8e5c94ecf6c224675b1
an.ngo <phowflowf135@gmail.com>
2021-09-16 16:49:25 +0700

    m

---
+++ src/showPicture.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class showPicture {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("1. Print the rectangle");
+        System.out.println("2. Print the square triangle");
+        System.out.println("3. Print isosceles triangle");
+        System.out.println("4. Exit");
+        int choice=-1;
+        while(choice!=4){
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Draw the square");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 2:
+                    System.out.println("Draw square triangle");
+                    System.out.println("* *");
+                    System.out.println("*");
+                case 3:
+                    System.out.println("Draw iso triangle");
+                case 4:
+                    System.exit(0);
+                default:
+                    break;
+            }
+        }
+    }
+}
